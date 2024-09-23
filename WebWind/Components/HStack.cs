60ced9ce9e3647f8e31cd78120b1875b95d6931d
Yannick Schössow <102901459+YanSchw@users.noreply.github.com>
2024-09-23 20:57:07 +0200

    Added HStack

---
+++ WebWind/Components/HStack.cs
@@ -0,0 +1,13 @@
+﻿using WebWind.Core;
+
+namespace WebWind.Components;
+
+public class HStack : Component
+{
+
+    public HStack()
+    {
+        JS.CreateDocumentElement("h-stack", Id);
+    }
+
+}
